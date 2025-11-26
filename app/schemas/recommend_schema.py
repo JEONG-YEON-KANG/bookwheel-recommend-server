1a74ca83c630a1620dc898a09d2533c975f306d8
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-11-26 17:27:19 +0900

    fix: Add score to response schema

--- app/schemas/recommend_schema.py
+++ app/schemas/recommend_schema.py
@@ -1,15 +1,25 @@
 from pydantic import BaseModel
 from typing import List
 
+
 # 공통 응답 정의
+class BookRecommendation(BaseModel):
+    bookIdx: int
+    score: float
+
+
 class BookListResponse(BaseModel):
-    recommendedBookList : List[int]
+    recommendedBookList: List[BookRecommendation]
+
 
 class UserListResponse(BaseModel):
-    recommendedUserList : List[int]
+    recommendedUserList: List[int]
+
 
 # 개별 요청 (body) 정의
-# 설문 기반 초기 추천 
+# 설문 기반 초기 추천
 class RecommendInitRequest(BaseModel):
-    genreList : List[int]
-    bookIdxList : List[int]
+    genreList: List[int] = []
+    moodList: List[int] = []
+    purposeList: List[int] = []
+    bookIdxList: List[int] = []
