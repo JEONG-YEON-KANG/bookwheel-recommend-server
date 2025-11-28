100c527219dcbbb134afc3c60bba660d335f755b
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-11-28 20:35:58 +0900

    fix: Change the schema according to changed return value

--- app/schemas/recommend_schema.py
+++ app/schemas/recommend_schema.py
@@ -1,31 +1,42 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import List
+from typing import List, Optional
 
 
-# 공통 응답 정의
-class BookRecommendation(BaseModel):
+# HomeRsponse의 내부 항목 정의
+class HomeBookItem(BaseModel):
     book_idx: int = Field(..., serialization_alias="bookIdx")
-    score: float
 
 
-class BookListResponse(BaseModel):
-    recommend_book_list: List[BookRecommendation] = Field(
-        ..., serialization_alias="recommendBookList"
+# 장르 섹션 정의
+class GenreSection(BaseModel):
+    genre: str
+    book_list: List[HomeBookItem] = Field(..., serialization_alias="bookList")
+
+
+# 홈 화면 최종 응답 스키마
+class HomeResponse(BaseModel):
+    # warm user
+    personal_top1: Optional[HomeBookItem] = Field(
+        None, serialization_alias="personalTop1"
     )
-
-
-class UserListResponse(BaseModel):
-    recommended_user_list: List[int] = Field(
-        ..., serialization_alias="recommendedUserList"
+    personal_top10: Optional[List[HomeBookItem]] = Field(
+        None, serialization_alias="personalTop10"
     )
-
-
-# 개별 요청 (body) 정의
-# 설문 기반 초기 추천
-class RecommendInitRequest(BaseModel):
-    genre_list: List[int] = Field(default=[], serialization_alias="genreList")
-    mood_list: List[int] = Field(default=[], serialization_alias="moodList")
-    purpose_list: List[int] = Field(default=[], serialization_alias="purposeList")
-    book_idx_list: List[int] = Field(default=[], serialization_alias="bookIdxList")
+    # cold user
+    initial_top1: Optional[HomeBookItem] = Field(
+        None, serialization_alias="initialTop1"
+    )
+    initial_top10: Optional[List[HomeBookItem]] = Field(
+        None, serialization_alias="initialTop10"
+    )
+    # recent 이력 없을 시 누락
+    recent_top10: Optional[List[HomeBookItem]] = Field(
+        None, serialization_alias="recentTop10"
+    )
+    # 공통
+    popular_top10: List[HomeBookItem] = Field(..., serialization_alias="popularTop10")
+    genre_section_list: List[GenreSection] = Field(
+        ..., serialization_alias="genreSectionList"
+    )
 
     model_config = ConfigDict(populate_by_name=True)
