a91134973f32d2f0ba0aa390eca5451b96e24dcb
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-12-06 22:27:47 +0900

    fix: Add the code about init.sql

--- init/init.sql
+++ init/init.sql
@@ -2,6 +2,8 @@ CREATE USER bookwheel_admin WITH PASSWORD '1234';
 
 CREATE DATABASE bookwheel OWNER bookwheel_admin;
 
+GRANT ALL ON SCHEMA public TO bookwheel_admin;
+
 \c bookwheel bookwheel_admin
 
 \i /rdb/ddl.sql
